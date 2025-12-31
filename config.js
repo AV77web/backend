8c0729d4b64a5009a5c018333551983319015677
andrea <villari.andrea@libero.it>
2025-12-31 14:40:23 +0100

    modificato url per regole cors per forntend in vercel nel file config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 const FRONTEND_URL = isProduction
-    ? "https://frontend-lyart-eight-34.vercel.app"
+    ? "https://frontend-phi-three-71.vercel.app/"
     : "http://localhost:5173";
 
 module.exports = {
